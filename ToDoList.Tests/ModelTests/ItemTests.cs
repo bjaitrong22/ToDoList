c61e88cc72f833f67afe3aff458d3d8da89a0d81
Bai Jaitrong <bjaitrong@comcast.net>
2022-11-12 14:25:03 -0700

    Add auto setter and static variable and method.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using ToDoList.Models;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests: IDisposable
   {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_item()
     {
@@ -25,6 +32,34 @@ namespace ToDoList.Tests
 
       //Assert
       Assert.AreEqual(description,result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updateDescription = "Do the dishes";
+      newItem.Description = updateDescription;
+      string result = newItem.Description;
+
+      Assert.AreEqual(updateDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      //Arrange
+      List<Item> newList = new List<Item> {};
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
     }
   } 
 }
