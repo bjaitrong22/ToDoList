234705c1b9292317fa0ae0c14d82027843e4a42e
Bai Jaitrong <bjaitrong@comcast.net>
2022-11-11 18:03:06 -0700

    Add outside packages information in .csproj files.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 
-namespace ToDoList.TestTools
+namespace ToDoList.Tests
 {
   [TestClass]
   public class ItemTests
   {
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item),newItem.GetType());
+    }
 
   } 
 }
