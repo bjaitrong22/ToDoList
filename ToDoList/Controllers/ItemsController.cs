57cc7f8fad1ce6fabae580f1c4d44ee0d1754d4d
Bai Jaitrong <bjaitrong@comcast.net>
2022-12-14 11:28:11 -0700

    Add update EditFunctionality.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,28 +1,74 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController: Controller
   {
-    
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
     {
-      Category category = Category.Find(categoryId);
-      return View(category);
+      _db = db;
     }
 
-    [HttpGet("/categories/{categoryId}/items/{itemId}")] 
-    public ActionResult Show(int categoryId, int itemId)
+    public ActionResult Index()
     {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
+      List<Item> model = _db.Items.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View (thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+      _db.Items.Update(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+    
+  //   [HttpGet("/categories/{categoryId}/items/new")]
+  //   public ActionResult New(int categoryId)
+  //   {
+  //     Category category = Category.Find(categoryId);
+  //     return View(category);
+  //   }
+
+  //   [HttpGet("/categories/{categoryId}/items/{itemId}")] 
+  //   public ActionResult Show(int categoryId, int itemId)
+  //   {
+  //     Item item = Item.Find(itemId);
+  //     Category category = Category.Find(categoryId);
+  //     Dictionary<string, object> model = new Dictionary<string, object>();
+  //     model.Add("item", item);
+  //     model.Add("category", category);
+  //     return View(model);
+  //   }
   }
 }
