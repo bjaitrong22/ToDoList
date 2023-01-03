4865f19db085f614fe67c13de4f399502127ab45
Bai Jaitrong <bjaitrong@comcast.net>
2023-01-03 12:29:28 -0700

    Remove old code.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -77,23 +77,5 @@ namespace ToDoList.Controllers
       
       return View(thisItem);
     }
-    
-  //   [HttpGet("/categories/{categoryId}/items/new")]
-  //   public ActionResult New(int categoryId)
-  //   {
-  //     Category category = Category.Find(categoryId);
-  //     return View(category);
-  //   }
-
-  //   [HttpGet("/categories/{categoryId}/items/{itemId}")] 
-  //   public ActionResult Show(int categoryId, int itemId)
-  //   {
-  //     Item item = Item.Find(itemId);
-  //     Category category = Category.Find(categoryId);
-  //     Dictionary<string, object> model = new Dictionary<string, object>();
-  //     model.Add("item", item);
-  //     model.Add("category", category);
-  //     return View(model);
-  //   }
   }
 }
