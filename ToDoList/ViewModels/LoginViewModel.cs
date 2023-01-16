8838850a6a421f40b16f01d83d9107eef242115c
Bai Jaitrong <bjaitrong@comcast.net>
2023-01-15 17:11:22 -0700

    Add Login action and view.

---
+++ ToDoList/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.ViewModels
+{
+  public class LoginViewModel
+  {
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email Address: ")]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password: ")]
+    public string Password { get; set; }
+
+
+  }
+}
+
